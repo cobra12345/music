5a3f420b190dad7aac842f4a2f0de63e65bf46c7
cobra12345 <41684611+cobra12345@users.noreply.github.com>
2024-10-16 15:59:15 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -1,4 +1,3 @@
-
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
@@ -8,22 +7,19 @@ module.exports = {
   options: [],
   run: async (client, interaction) => {
     try {
-
       const supportServerLink = "https://discord.gg/Y3fvqDAQX7";
-        const embed = new EmbedBuilder()
-            .setColor('#b300ff')
-            .setAuthor({
-              name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/Y3fvqDAQX7'
-          })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}`),
+      const embed = new EmbedBuilder()
+        .setColor('#b300ff')
+        .setAuthor({
+          name: 'Support Server',
+          iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&',
+          url: 'https://discord.gg/Y3fvqDAQX7'
+        })
+        .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}`);
 
       return interaction.reply({ embeds: [embed] });
     } catch (e) {
-    console.error(e); 
-  }
+      console.error(e);
+    }
   },
 };
-
-
